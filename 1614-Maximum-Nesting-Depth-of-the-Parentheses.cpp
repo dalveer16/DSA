92d8f70ead169e9470ef1ed23a6a139c3ca266c0
Dalveer Singh <57098009+dalveer16@users.noreply.github.com>
2024-04-04 10:30:18 +0530

    Create 1614-Maximum-Nesting-Depth-of-the-Parentheses.cpp

---
+++ 1614-Maximum-Nesting-Depth-of-the-Parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        if(s=="") return 0;
+        int maxDepth=0;
+        int cntOpen=0;
+        for( int i=0;i<s.size();i++){
+
+            if(s[i]=='('){
+                cntOpen++;
+                maxDepth=max(maxDepth,cntOpen);
+            }
+            else if(s[i]==')'){
+             cntOpen--;
+            }
+            else{
+               continue;
+            }
+        }
+       return maxDepth; 
+    }
+};
